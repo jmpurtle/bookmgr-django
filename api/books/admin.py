a9586b4d189bed7d286b8fa026ce5f9b0f318e58
John Purtle <purtlejohnmark@gmail.com>
2019-07-12 19:28:53 -0500

    Added Books model

--- api/books/admin.py
+++ api/books/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Book, Genre, Subject
 
-# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Book._meta.fields]
+
+admin.site.register(Book, BookAdmin)
--- api/books/models.py
+++ api/books/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Book(models.Model):
+	title = models.CharField(max_length=512)
+	subtitle = models.CharField(max_length=512)
+	author = models.CharField(max_length=100)
+	isbn = models.CharField(max_length=50)
+	summary = models.TextField()
+	description = models.TextField()
+	publish_date = models.DateField()
+	publisher = models.CharField(max_length=100)
+	book_format = models.CharField(max_length=100)
+	pages = models.IntegerField()
+	loc_class = models.CharField(max_length=100)
+	loc_ctrl = models.CharField(max_length=100)
+	dewey = models.CharField(max_length=100)
+	country = models.CharField(max_length=150)
+	lang = models.CharField(max_length=30)
+	book_status = models.CharField(max_length=50)
+	cover = models.FileField()
+	created = models.DateTimeField(default=timezone.now)
+	updated = models.DateTimeField(auto_now=True)
+
+class Genre(models.Model):
+	name = models.CharField(max_length=100)
+	books = models.ManyToManyField(Book, related_name="genres")
+
+class Subject(models.Model):
+	name = models.CharField(max_length=100)
+	books = models.ManyToManyField(Book, related_name="subjects")
